d80bc56d850e6b6c8bbc13c47399711085d36d34
lavanshu5030 <lavanshu2001@gmail.com>
2023-12-04 18:54:11 +0530

    Running spring boot application with dependencies

---
+++ SpringBootDependenciesDemo/src/main/java/com/example/SpringActuatorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorDemoApplication.class, args);
+	}
+
+}
